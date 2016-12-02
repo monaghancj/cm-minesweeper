f3246165ea1e51685fb7b19a93673d27c68a29bf
Cameron <monaghancj@g.cofc.edu>
2016-12-02 14:50:16 -0500

    Finished functionality of game

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ const App = React.createClass({
   getInitialState: function() {
     return {
       grid: [],
-      board: {}
+      board: {},
+      state: null,
+      blewUp: false
     }
   },
 
@@ -19,7 +21,10 @@ const App = React.createClass({
         strColHead = '   ',
         grid = board.grid();
 
-    this.setState({grid})
+    this.setState({
+      grid: grid,
+      board: board
+    })
 
     // print a header that shows the column numbers
     for (i = 0; i < board.numCols(); i++) {
@@ -72,32 +77,66 @@ const App = React.createClass({
       mines: 15
     })
     var board = new minesweeper.Board(mineArray)
-    this.setState({board})
     this.printBoard(board)
   },
 
   handleClick(cell) {
-    var self = this
-    return function() {
-      console.log('Cell: ' + JSON.stringify(cell))
-      self.state.board.openCell(cell.x, cell.y)
-    }
+    return (e) => {
+      if  (cell.isMine) {
+        this.state.board.openCell(cell.x, cell.y)
+        this.setState({blewUp: true})
+      }
+      else {
+        this.state.board.openCell(cell.x, cell.y)
+        this.setState({board: this.state.board})
+      }
 
+    }
+  },
+
+  again() {
+    var mineArray = minesweeper.generateMineArray({
+      rows: 10,
+      cols: 10,
+      mines: 15
+    })
+    var board = new minesweeper.Board(mineArray)
+    this.printBoard(board)
+    this.setState({blewUp:false})
   },
 
   render() {
+    const mineFunction = (cell) => {
+      if (this.state.blewUp && cell.isMine) {
+        return '💣'
+      } else if (cell.state === 1 && cell.isMine) {
+        return '💣'
+      } else if (cell.state === 1 && !cell.isMine) {
+        return JSON.stringify(cell.numAdjacentMines)
+      }
+    }
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div className="ma3 helvetica center mw7-ns">
+        <h1 className="fw1 center"> Afgan Sweeper  <span className="green">monaghancj</span></h1>
+        {this.state.blewUp
+          ? <div>
+            <h2 className="fw1 center">You Lose</h2>
+            <a
+              className="f6 link dim br-pill ba ph3 pv2 mb2 dib green"
+              onClick={this.again}>
+                Play Again
+            </a>
+          </div> : null
+        }
         <div className="">
           {this.state.grid.map( row => {
             return <div className="fl w-100">
               {row.map( cell => {
                 return <div
-                        onClick={this.handleClick(cell)}
-                        className="bg-green ba w2 h2 fl ib">
-                          {JSON.stringify(cell.isMine)}
-                       </div>
+                          className={cell.state === 0 ? "w2 h2 fl ib mb1 mr1 br1 bg-green" : "w2 h2 fl ib mb1 mr1 br1 bg-light-yellow"}
+                          onClick={this.handleClick(cell)}>
+                    <p className="ml2">{ mineFunction(cell) }</p>
+                </div>
               })}
             <br />
             </div>
